f5f29058b952203fe49075bf8c9176c231e8e980
Daniel Lezcano <daniel.lezcano727@gmail.com>
2022-03-22 17:02:27 -0300

    Se borran comentarios

--- code/threads/thread_test_garden_sem.cc
+++ code/threads/thread_test_garden_sem.cc
@@ -1,10 +1,3 @@
-/// Copyright (c) 1992-1993 The Regents of the University of California.
-///               2007-2009 Universidad de Las Palmas de Gran Canaria.
-///               2016-2021 Docentes de la Universidad Nacional de Rosario.
-/// All rights reserved.  See `copyright.h` for copyright notice and
-/// limitation of liability and disclaimer of warranty provisions.
-
-
 #include "thread_test_garden.hh"
 #include "system.hh"
 #include "semaphore.hh"
@@ -49,9 +42,6 @@ ThreadTestGardenSem()
         t->Fork(Turnstile, (void *) n);
     }
 
-    // Wait until all turnstile threads finish their work.  `Thread::Join` is
-    // not implemented at the beginning, therefore an ad-hoc workaround is
-    // applied here.
     for (unsigned i = 0; i < NUM_TURNSTILES; i++) {
         while (!done[i]) {
             currentThread->Yield();
